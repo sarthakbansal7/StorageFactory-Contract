d4366322563349718aed0059150d62f2d7ca4c8d
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-18 15:37:37 +0530

    Add functions to interact with SimpleStorage.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -14,5 +14,21 @@ contract StorageFactory {
         listOfSimpleStorageContracts.push(simpleStorageContractVariable);
     }
 
+   function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(
+            _simpleStorageNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+
     
 }
