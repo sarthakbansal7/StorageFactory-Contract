f94474a1bf71ca7045111a6e29e9e65fe766f492
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-18 15:34:59 +0530

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.19;
+
+// import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    
+}
